193b66d8b4fee89a2559c3564006e0e72ad3ac9d
Lukecp <lukecp@pm.me>
2020-10-06 22:00:49 -0400

    completed MVP

--- index.js
+++ index.js
@@ -39,9 +39,26 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+	this.stomach = [];
+};
 
-}
+Person.prototype.eat = function (food) {
+	if (this.stomach.length < 10) {
+		this.stomach.push(food);
+	};
+};
+
+Person.prototype.poop = function () {
+	this.stomach = [];
+};
+
+Person.prototype.toString = function () {
+	return `${this.name}, ${this.age}`;
+};
+
 
 /*
   TASK 2
@@ -57,8 +74,15 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model, milesPerGallon) {
+	this.model = model;
+	this.milesPerGallon = milesPerGallon;
+	this.tank = 0;
+	this.odometer = 0;
+}
 
+Car.prototype.fill = function (gallons) {
+	this.tank += gallons;
 }
 
 /*
@@ -68,18 +92,25 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+	Person.call(this, name, age);
+	this.favoriteToy = favoriteToy
+}
 
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function () {
+	return `Playing with ${this.favoriteToy}`
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window Binding is when "this" is used in the global scope. It will refer to the window object.
+  2. Implicit Binding is when "this" is used in an object. In this case, "this" refers to the object (or its immediate parent).
+  3. New Binding is when "this" is used in a constructor function. "this" will refer to the specific instance of the object created by the constructor.
+  4. Explicit Binding happens when we use the .call or .apply methods to call an object, but we pass the .call or .apply methods another object. This will basically switch out the property values with the object passed in. This can be a way to "borrow" methods from other objects.
 */
 
 
